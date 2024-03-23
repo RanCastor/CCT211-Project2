562f842fa072da5941f8837509674fcaad9c3c96
RanCastor <erengulenolgu0316@gmail.com>
2024-03-23 00:27:53 -0400

    You can build on this app
    Hey
    
    I just created something for you to use. There are new widgets that you can use, also you can create your own. I used ChatGPT to create that big textbox.
    
    Play around. Have fun! ;)

--- v1/main.py
+++ v1/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import *
 from tkinter import font as tkfont
+from widgets import EntryField, Combo, RadiobuttonField, ScrolledTextWidget
 import tkinter.ttk as ttk  # just for treeview
 # import entry_field  # no particular good reason I did it the other way here
 # from models import *  # done this way to access classes just by name
@@ -28,10 +30,10 @@ class App(tk.Tk):
 
         self.frames = {}
         # for each other custom Frame class you make, you could add it to this tuple
-        for F in ("""SomeBrowsePage""", """SomeReadPage""", """SomeCreatePage"""):
+        for F in (StarterBrowsePage, TestPage):
             page_name = F.__name__
             # last arg - send the object that accesses the db
-            frame = F(parent=container, controller=self, persist=self.data)
+            frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
 
             # put all of the pages in the same location;
@@ -39,7 +41,7 @@ class App(tk.Tk):
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("SomeBrowsePage")
+        self.show_frame("StarterBrowsePage")
 
     def show_frame(self, page_name, rid=0):
         '''Show a frame for the given page name'''
@@ -53,7 +55,7 @@ class App(tk.Tk):
         frame.tkraise()
 
 
-class SomeBrowsePage(tk.Frame):
+class StarterBrowsePage(tk.Frame):
     ''' the Browse page must show all the items in the database and allow
     access to editing and deleting, as well as the ability to go to a screen
     to add new ones. This is the 'home' screen.
@@ -68,6 +70,64 @@ class SomeBrowsePage(tk.Frame):
         #  This includes creating a Treeview inside this frame. This means we should have methods such as
         #  edit_selected, on_select, delete_selected, update. Do not get frightened. We can use the demo's code
         #  to write these methods.
+
+        # Left Frame
+        left_frame_pad = 15
+        self.left_frame = tk.Frame(self, relief="ridge", borderwidth=5)
+        self.left_frame.grid(row=0, column=0, sticky="nsew")
+        self.right_frame = tk.Frame(self, relief="ridge", borderwidth=5)
+        self.right_frame.grid(row=0, column=1, sticky="nsew")
+
+        self.name = EntryField(self.left_frame, label="Name")
+        self.name.grid(row=0, column=0, sticky="nsew", pady=left_frame_pad)
+
+        self.studentID = EntryField(self.left_frame, label="Studend ID")
+        self.studentID.grid(row=1, column=0, sticky="nsew", pady=left_frame_pad)
+
+        self.year = Combo(self.left_frame, label='Birth Year', options=('2003', '2004', '2005'))
+        self.year.grid(row=3, column=0, sticky="nsew", pady=left_frame_pad)
+
+        self.radiobutton_field = RadiobuttonField(self.left_frame, label="Year:", options=["Year 1", "Year 2", "Year 3", "Year 4+"])
+        self.radiobutton_field.grid(row=4, column=0, sticky="nsew")
+
+        self.button1 = Button(self.left_frame, text="Print Radio Button", width=20, command=lambda: {print(self.radiobutton_field.get())})
+        self.button1.grid(row=5, column=0, sticky="nsew", pady=left_frame_pad)
+
+        self.mature = RadiobuttonField(self.left_frame, label="Mature Student:",
+                                                  options=["Yes", "No"])
+        self.mature.grid(row=6, column=0, sticky="nsew", pady=left_frame_pad)
+
+        self.accessibility = RadiobuttonField(self.left_frame, label="Registered with accessibility:",
+                                       options=["Yes", "No"])
+        self.accessibility.grid(row=7, column=0, sticky="nsew", pady=left_frame_pad)
+
+        self.text_area = ScrolledTextWidget(self.left_frame, label="Enter Text:")
+        self.text_area.grid(row=8, column=0)
+
+        self.button1 = Button(self.left_frame, text="Print Scrolled Text", width=20,
+                              command=lambda: {print(self.text_area.get())})
+        self.button1.grid(row=9, column=0, sticky="nsew", pady=left_frame_pad)
+
+        # Right Frame
+        self.someother_label = tk.Label(self.right_frame, text="something")
+        self.someother_label.grid(row=0, column=0, sticky="nsew")
+
+        self.grid_rowconfigure(0, weight=1)  # This makes lower edge of frames touch the bottom of the screen
+
+
+class TestPage(tk.Frame):
+    ''' the Browse page must show all the items in the database and allow
+    access to editing and deleting, as well as the ability to go to a screen
+    to add new ones. This is the 'home' screen.
+    '''
+
+    def __init__(self, parent, controller, persist=None):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.test_label = tk.Label(self, text="something")
+        self.test_label.grid(row=0, column=0, sticky="nsew")
+
 
 if __name__ == "__main__":
     app = App()
---
+++ v1/widgets.py
@@ -0,0 +1,93 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import sys
+from tkinter import scrolledtext
+
+
+class EntryField(tk.Frame):
+    def __init__(self, parent, label='', passwordField=False, *args, **kwargs):
+        # keep in mind EntryField is a Frame widget so any args and kwargs apply only to it, not its children - the label and title
+        super().__init__(parent, *args, **kwargs)
+        self.dataentry = tk.StringVar()
+        self.label = label
+
+        self.title = tk.Label(self, text=label, width=10)
+        self.title.grid(row=0, column=0, padx=10, sticky=(tk.W + tk.E))
+        # args and kwargs are only sent in the super and this doesn't apply to the super construct but one of the children
+        if passwordField:
+            self.field = tk.Entry(
+                self, width=30, textvariable=self.dataentry, show="*")
+        else:
+            self.field = tk.Entry(
+                self, width=30, textvariable=self.dataentry)
+        self.field.grid(row=0, column=1, padx=15, sticky=(tk.W + tk.E))
+
+    def reset(self):
+        self.dataentry.set("")
+
+    def get(self):
+        return self.dataentry.get()
+
+
+class Combo(tk.Frame):
+    def __init__(self, parent, label='', options=('sasdf'), *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.dataentry = tk.StringVar()
+        self.label = label
+
+        self.title = tk.Label(self, text=label, width=10)
+        self.title.grid(row=0, column=0, padx=10, sticky=(tk.W + tk.E))
+        self.combo = ttk.Combobox(self, width=15, textvariable=self.dataentry, state="readonly")
+        self.combo['values'] = options
+        self.combo.grid(row=0, column=1, padx=15, sticky=(tk.W + tk.E))
+        self.combo.current()
+
+    def reset(self):
+        self.dataentry.set("")
+
+    def get(self):
+        return self.dataentry.get()
+
+
+class RadiobuttonField(tk.Frame):
+    def __init__(self, parent, label='', options=[], *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.choice = tk.StringVar()
+
+        self.label = tk.Label(self, text=label, width=25)
+        self.label.grid(row=0, column=0, padx=10, sticky=(tk.W + tk.E))
+
+        self.option_buttons = []
+        for index, option in enumerate(options):
+            button = tk.Radiobutton(self, text=option, variable=self.choice, value=option)
+            button.grid(row=0, column=index + 1, padx=5, sticky=(tk.W + tk.E))
+            self.option_buttons.append(button)
+
+    def reset(self):
+        self.choice.set("")
+
+    def get(self):
+        return self.choice.get()
+
+
+class ScrolledTextWidget(tk.Frame):
+    def __init__(self, parent, label='', font=None, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.label = label
+
+        if font is not None:
+            self.custom_font = font
+        else:
+            self.custom_font = ("Arial", 10)  # Default font if not provided
+
+        self.label = tk.Label(self, text=label, width=10)
+        self.label.grid(row=0, column=0, padx=10, sticky=tk.W)
+
+        self.scrolled_text = scrolledtext.ScrolledText(self, width=45, height=5, font=self.custom_font)
+        self.scrolled_text.grid(row=0, column=1, padx=10, pady=5, sticky=tk.W)
+
+    def reset(self):
+        self.scrolled_text.delete("1.0", tk.END)
+
+    def get(self):
+        return self.scrolled_text.get("1.0", tk.END).strip()
