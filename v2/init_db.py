bbd8508c578ce1403150b174671e5c082373d73e
RanCastor <erengulenolgu0316@gmail.com>
2024-04-03 16:03:22 -0400

    init.py limitation CHAR
    I added elements such as CHAR(10) in order to indicate expected data length to init.py.

--- v2/init_db.py
+++ v2/init_db.py
@@ -14,8 +14,8 @@ def create_database():
     cur.execute('''DROP TABLE IF EXISTS tickets;''')
     cur.execute('''CREATE TABLE IF NOT EXISTS "tickets" (
             "ticket_id" INTEGER PRIMARY KEY,
-            "name" TEXT NOT NULL CHECK (name != ''),
-            "student_id" INTEGER NOT NULL CHECK (length(student_id) = 10),
+            "name" CHAR(1000) NOT NULL CHECK (name != '' AND length(name) <= 1000),
+            "student_id" CHAR(10) NOT NULL CHECK (length(student_id) = 10),
             "date" DATE NOT NULL CHECK (date != ''),
             "program" TEXT NOT NULL CHECK (program != ''),
             "study_year" TEXT NOT NULL CHECK (study_year != ''),
@@ -24,16 +24,16 @@ def create_database():
             "summary" TEXT NOT NULL CHECK (summary != '')
             );''')
     # cur.execute('''CREATE TABLE IF NOT EXISTS "tickets" (
-    #         "ticket_id"	INTEGER PRIMARY KEY,
-    #         "name"	TEXT NOT NULL,
-    #         "student_id" INTEGER NOT NULL,
-    #         "date"	DATE NOT NULL,
-    #         "program" TEXT NOT NULL,
-    #         "study_year" TEXT NOT NULL,
-    #         "accessibility" TEXT NOT NULL,
-    #         "category"	TEXT NOT NULL,
-    #         "summary" TEXT NOT NULL
-    #         );''')
+    #             "ticket_id" INTEGER PRIMARY KEY,
+    #             "name" CHAR(1000) NOT NULL CHECK (name != '' AND length(name) <= 1000),
+    #             "student_id" INTEGER NOT NULL CHECK (length(student_id) = 10),
+    #             "date" DATE NOT NULL CHECK (date != ''),
+    #             "program" TEXT NOT NULL CHECK (program != ''),
+    #             "study_year" TEXT NOT NULL CHECK (study_year != ''),
+    #             "accessibility" TEXT NOT NULL CHECK (accessibility != ''),
+    #             "category" TEXT NOT NULL CHECK (category != ''),
+    #             "summary" TEXT NOT NULL CHECK (summary != '')
+    #             );''')
     # check lecture slides from week 11 to insert already existing data by cur.execute
     # Manually inserting data
     cur.executescript("""
